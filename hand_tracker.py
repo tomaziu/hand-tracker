05ad555c6eae688b53ec8e39df03454febbe09ae
tommaziu <thomazdemoraisnunes2018@gmail.com>
2026-06-22 21:24:28 -0300

    fix: nomes corretos dos parametros de confianca

--- hand_tracker.py
+++ hand_tracker.py
@@ -63,8 +63,8 @@ class HandTracker:
             base_options=base_options,
             num_hands=2,
             running_mode=vision.RunningMode.VIDEO,
-            min_detection_confidence=0.7,
-            min_tracking_confidence=0.6
+            min_hand_detection_confidence=0.7,
+            min_hand_tracking_confidence=0.6
         )
         self.hand_landmarker = vision.HandLandmarker.create_from_options(options)
         
